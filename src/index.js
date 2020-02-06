4699cdb7220fb3f5104a7b1f484d72914e0f6a33
Filip Jerga <jerga99@gmail.com>
2020-02-06 09:15:23 +0100

    2 Folder Structure of Project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// JSX javascript XML
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
