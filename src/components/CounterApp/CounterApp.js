e8d9ddee2c06a36ba75d9a3f453ffb1b63e0d976
Filip Jerga <jerga99@gmail.com>
2020-02-06 17:56:27 +0100

    6 Counter App State

--- src/components/CounterApp/CounterApp.js
+++ src/components/CounterApp/CounterApp.js
@@ -1,23 +1,27 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import './CounterApp.css';
 
 const CounterApp = () => {
+  const [count, setCount] = useState(0);
 
-  const increment = () => {
-    alert('Incrementing!');
-  }
+  // const stateArray = useState(100);
+  // const mySuperValue = stateArray[0];
+  // const changeMySuperValue = stateArray[1];
+  
+  // const increment = (step) => {
+  //   setCount(count + step);
+  // }
 
-  const decrement = () => {
-    alert('Decrementing!');
-  }
+  const increment = (step) => () => setCount(count + step)
+  
 
   return (
     <div>
       <div className="counter-app">
-        <h1 className="value">0</h1>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
+        <h1 className="value">{count}</h1>
+        <button onClick={increment(1)}>Increment</button>
+        <button onClick={increment(-10)}>Decrement</button>
       </div>
     </div>
   )
