bd264223fb1115f63fcf7213e17289141db4d145
Filip Jerga <jerga99@gmail.com>
2020-02-07 15:35:20 +0100

    14 How Everything works part 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import CounterApp from './components/CounterApp/CounterApp'
 
+// Explaining how everyhing works (:
 const App = () => {
   return (
     <CounterApp title="My Counter Application"/>
