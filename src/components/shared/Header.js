65d01a71d40b6d016c9f88957e0b258934880996
Filip Jerga <jerga99@gmail.com>
2020-02-10 10:55:26 +0100

    17 Navbar Stylings

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -6,7 +6,7 @@ import React from 'react';
 const Header = () => {
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <a className="navbar-brand" href="#">BookWithMe</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -14,8 +14,8 @@ const Header = () => {
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <input className="form-control mr-sm-2 bwm-search" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-bwm-main btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
