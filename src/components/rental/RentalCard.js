2abda37fbaa42bfc7f159bb0ae976225ab25acff
Filip Jerga <jerga99@gmail.com>
2020-02-10 16:24:28 +0100

    20 Iterate Rentals + Rental Card

---
+++ src/components/rental/RentalCard.js
@@ -0,0 +1,24 @@
+
+
+import React from 'react';
+
+const RentalCard = ({rental}) => {
+
+  return (
+    <div className="card bwm-card">
+      <img 
+        className="card-img-top" 
+        src={rental.image}
+        alt={rental.title} />
+      <div className="card-body">
+        <h6 className="card-subtitle mb-0 text-muted">
+          {rental.shared ? 'Shared' : 'Whole'} {rental.category} &#183; {rental.city}
+        </h6>
+        <h5 className="card-title big-font">{rental.title}</h5>
+        <p className="card-text">${rental.dailyPrice} per Night &#183; Free Cancelation</p>
+      </div>
+    </div>
+  )
+}
+
+export default RentalCard;
--- src/pages/RentalHome.js
+++ src/pages/RentalHome.js
@@ -1,46 +1,65 @@
 
 
 import React from 'react';
+import RentalCard from '../components/rental/RentalCard';
 
 class RentalHome extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      rentals: [{
+        _id: '1',
+        title: "Nice view on ocean",
+        city: "San Francisco",
+        category: "condo",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 4,
+        shared: true,
+        description: "Very nice apartment in center of the city.",
+        dailyPrice: 43
+      },
+      {
+        _id: '2',
+        title: "Modern apartment in center",
+        city: "New York",
+        category: "apartment",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 1,
+        shared: false,
+        description: "Very nice apartment in center of the city.",
+        dailyPrice: 11
+      },
+      {
+        _id: '3',
+        title: "Old house in nature",
+        city: "Bratislava",
+        category: "house",
+        image: "http://via.placeholder.com/350x250",
+        numOfRooms: 5,
+        shared: true,
+        description: "Very nice apartment in center of the city.",
+        dailyPrice: 23
+      }]
+    }
+  }
 
+  renderRentals = (rentals) => 
+    rentals.map(rental => 
+      <div key={rental._id} className="col-md-3">
+        <RentalCard rental={rental}/>
+      </div>
+    );
+  
   render() {
+    const { rentals } = this.state;
+
     return (
       <div className="card-list">
         <div className="container">
           <h1 className="page-title">Your Home All Around the World</h1>
           <div className="row">
-            <div className="col-md-3">
-              <div className="card bwm-card">
-                <img className="card-img-top" src="http://via.placeholder.com/350x250" alt="Card image cap" />
-                <div className="card-body">
-                  <h6 className="card-subtitle mb-0 text-muted">Whole Apartment &#183; Bratislava</h6>
-                  <h5 className="card-title big-font">Awesome Place</h5>
-                  <p className="card-text">$23 per Night &#183; Free Cancelation</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div className="card bwm-card">
-                <img className="card-img-top" src="http://via.placeholder.com/350x250" alt="Card image cap" />
-                <div className="card-body">
-                  <h6 className="card-subtitle mb-0 text-muted">Whole Apartment &#183; Bratislava</h6>
-                  <h5 className="card-title big-font">Awesome Place</h5>
-                  <p className="card-text">$23 per Night &#183; Free Cancelation</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div className="card bwm-card">
-                <img className="card-img-top" src="http://via.placeholder.com/350x250" alt="Card image cap" />
-                <div className="card-body">
-                  <h6 className="card-subtitle mb-0 text-muted">Shared Apartment &#183; Barcelona</h6>
-                  <h5 className="card-title big-font">Sunny Place</h5>
-                  <p className="card-text">$93 per Night &#183; Free Cancelation</p>
-                </div>
-              </div>
-            </div>
+            { this.renderRentals(rentals) }
           </div>
         </div>
       </div>
