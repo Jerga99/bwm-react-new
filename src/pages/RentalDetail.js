92416daf368b7ecb3fa5120efafbca00e9c7c058
Filip Jerga <jerga99@gmail.com>
2020-02-12 11:10:23 +0100

    42 Rental detail refactor

--- src/pages/RentalDetail.js
+++ src/pages/RentalDetail.js
@@ -1,10 +1,9 @@
 
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchRentalById } from 'actions';
-import { capitalize } from 'helpers/functions';
+import RentalInfo from 'components/rental/RentalInfo';
 
 
 class RentalDetail extends React.Component {
@@ -22,7 +21,6 @@ class RentalDetail extends React.Component {
         <div className="upper-section">
           <div className="row">
             <div className="col-md-6">
-              {/* <!-- TODO: Display rental image --> */}
               <img src={rental.image} alt={rental.title} />
             </div>
             <div className="col-md-6">
@@ -31,60 +29,10 @@ class RentalDetail extends React.Component {
             </div>
           </div>
         </div>
-
         <div className="details-section">
           <div className="row">
             <div className="col-md-8">
-              <div className="rental">
-                {/* <!-- TODO: Display shared category --> */}
-                <h2 className={`rental-type type-${rental.category}`}>
-                  {rental.shared ? 'Shared' : 'Whole'} {rental.category}
-                </h2>
-                {/* <!-- TODO: Display title --> */}
-                <h1 className="rental-title">{rental.title}</h1>
-                {/* <!-- TODO: Display city --> */}
-                <h2 className="rental-city">{capitalize(rental.city)}</h2>
-                <div className="rental-room-info">
-                  {/* <!-- TODO: Display numOfRooms --> */}
-                  <span><i className="fa fa-building"></i>{rental.numOfRooms} bedrooms</span>
-                    {/* // <!-- TODO: Display numOfRooms + 4 --> */}
-                  <span><i className="fa fa-user"></i> {rental.numOfRooms + 4} guests</span>
-                  {/* // <!-- TODO: Display numOfRooms + 2 --> */}
-                  <span><i className="fa fa-bed"></i> {rental.numOfRooms + 2} beds</span>
-                </div>
-                {/* <!-- TODO: Display description --> */}
-                <p className="rental-description">
-                  {rental.description}
-                </p>
-                <hr/>
-                <div className="rental-assets">
-                  <h3 className="title">Assets</h3>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <span>
-                        <FontAwesomeIcon icon="asterisk" /> Cooling
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="thermometer" /> Heating
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="location-arrow" /> Iron
-                      </span>
-                    </div>
-                    <div className="col-md-6">
-                      <span>
-                        <FontAwesomeIcon icon="desktop" /> Working area
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="cube" /> Washing machine
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="archive" /> Dishwasher
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <RentalInfo rental={rental} />
             </div>
             <div className="col-md-4"> BOOKING</div>
           </div>
