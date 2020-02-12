30be46f793e2c49a48ab11d1986a770a0a3d2f6a
Filip Jerga <jerga99@gmail.com>
2020-02-12 12:55:20 +0100

    46 Login Form Validation

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -2,36 +2,50 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 const LoginForm = ({onSubmit}) => {
 
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors } = useForm();
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group">
         <label htmlFor="email">Email</label>
         <input
-          ref={register}
+          ref={register({required: true, pattern: EMAIL_PATTERN})}
           name="email"
           type="email"
           className="form-control"
           id="email" />
-        {/* <div className="alert alert-danger">
-          <div>
-            Email is required.
+        { errors.email &&
+          <div className="alert alert-danger"> 
+            { errors.email.type === 'required' &&
+              <span>Email is required!</span>
+            }
+            { errors.email.type === 'pattern' &&
+              <span>Not valid email format!</span>
+            }
           </div>
-          <div>
-            Must be valid email format!
-          </div>
-        </div> */}
+        }
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
         <input 
-          ref={register}
+          ref={register({required: true, minLength: 8})}
           name="password"
           type="password"
           className="form-control"
           id="password" />
+        { errors.password &&
+          <div className="alert alert-danger"> 
+            { errors.password.type === 'required' &&
+              <span>Password is required!</span>
+            }
+            { errors.password.type === 'minLength' &&
+              <span>Minimum length of password is 8 characters!</span>
+            }
+          </div>
+        }
       </div>
       <button 
         type="submit" 
