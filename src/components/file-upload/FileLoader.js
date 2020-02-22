d198d6b4f8497dd21d43f6ed1dc0f17add41f837
Filip Jerga <jerga99@gmail.com>
2020-02-22 14:46:33 +0100

    127 Image Upload Status

--- src/components/file-upload/FileLoader.js
+++ src/components/file-upload/FileLoader.js
@@ -10,7 +10,8 @@ class FileLoader extends React.Component {
     this.fileReader = new FileReader();
     this.selectedImg = null;
     this.state = {
-      imgBase64: ''
+      imgBase64: '',
+      imgStatus: 'INIT'
     }
   }
 
@@ -23,17 +24,18 @@ class FileLoader extends React.Component {
   }
 
   handleImageUpload = () => {
-    uploadImage(this.selectedImg)
+    this.changeImageStatus('PENDING');
+    uploadImage('askjdn')
       .then(() => {
-        alert('Image Uploaded!');
+        this.changeImageStatus('UPLOADED');
       })
       .catch(() => {
-        alert('Upload Failed!');
+        this.changeImageStatus('ERROR');
       })
   }
 
   handleImageLoad = ({target: {result: imgBase64}}) => {
-    this.setState({ imgBase64 })
+    this.setState({ imgBase64, imgStatus: 'LOADED' })
   }
 
   listenToFileLoading = () => {
@@ -49,8 +51,10 @@ class FileLoader extends React.Component {
     this.fileReader.readAsDataURL(this.selectedImg);
   }
 
+  changeImageStatus= imgStatus => this.setState({imgStatus})
+
   render() {
-    const { imgBase64 } = this.state;
+    const { imgBase64, imgStatus } = this.state;
     return (
       <div className="img-upload-container">
         <label className="img-upload btn btn-bwm-main">
@@ -68,18 +72,37 @@ class FileLoader extends React.Component {
               <div className="img-preview">
                 <img src={imgBase64}></img>
               </div>
+              { imgStatus === 'PENDING' &&
+                <div className="spinner-container upload-status">
+                  Loading...
+                </div>
+              }
+              { imgStatus === 'UPLOADED' &&
+                <div className="alert alert-success upload-status">
+                  Image has been succesfuly uploaded!
+                </div>
+              }
+              { imgStatus === 'ERROR' &&
+                <div className="alert alert-danger upload-status">
+                  Image upload failed!
+                </div>
+              }
             </div>
-            <button
-              onClick={this.handleImageUpload}
-              className="btn btn-success mr-1"
-              type="button">
-              Upload
-            </button>
-            <button
-              className="btn btn-danger"
-              type="button">
-              Cancel
-            </button>
+            { imgStatus === 'LOADED' &&
+              <>
+                <button
+                  onClick={this.handleImageUpload}
+                  className="btn btn-success mr-1"
+                  type="button">
+                  Upload
+                </button>
+                <button
+                  className="btn btn-danger"
+                  type="button">
+                  Cancel
+                </button>
+              </>
+            }
           </>
         }
       </div>
