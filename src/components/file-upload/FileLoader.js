bc3a26dd4a0ebb09873fab096afd899f6574b826
Filip Jerga <jerga99@gmail.com>
2020-02-22 13:42:26 +0100

    124 File Reader

--- src/components/file-upload/FileLoader.js
+++ src/components/file-upload/FileLoader.js
@@ -4,12 +4,41 @@ import './FileLoader.scss';
 
 class FileLoader extends React.Component {
 
+  constructor() {
+    super();
+    this.fileReader = new FileReader();
+    this.state = {
+      imgBase64: ''
+    }
+  }
+
+  componentDidMount() {
+    this.listenToFileLoading();
+  }
+
+  componentWillUnmount() {
+    this.removeFileLoadListener();
+  }
+
+  handleImageLoad = ({target: {result: imgBase64}}) => {
+    this.setState({ imgBase64 })
+  }
+
+  listenToFileLoading = () => {
+    this.fileReader.addEventListener('load', this.handleImageLoad);
+  }
+
+  removeFileLoadListener = () => {
+    this.fileReader.removeEventListener('load', this.handleImageLoad);
+  }
+
   handleChange = event => {
-    debugger
     const file = event.target.files[0];
+    this.fileReader.readAsDataURL(file);
   }
 
   render() {
+    const { imgBase64 } = this.state;
     return (
       <div className="img-upload-container">
         <label className="img-upload btn btn-bwm-main">
@@ -21,6 +50,9 @@ class FileLoader extends React.Component {
             type="file"
             />
         </label>
+        { imgBase64 &&
+          <img src={imgBase64}></img>
+        }
       </div>
     )
   }
